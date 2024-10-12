afbaf3b61fc5bf81c5a8874715b49fa66fdc7b7f
Pro-ktmr <Pro.ktmr@gmail.com>
2024-10-12 15:37:45 +0900

    Deploy

---
+++ _next/static/7YK9Wmb5iPSXO29KlA56-/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[appId]","\u002F[appId]\u002F[textbookId]\u002F[textbookPageId]","\u002F[appId]\u002FgetTextbooks"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
